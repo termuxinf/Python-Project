3de3311328c3145410f336850dafe0c0110041d6
termuxinf <80803750+termuxinf@users.noreply.github.com>
2022-10-21 09:37:35 +0700

    Create biodata.py

---
+++ biodata.py
@@ -0,0 +1,17 @@
+def run():
+    nama = input('input nama: ')
+    alamat = input('input alamat: ')
+    ttl = input('input tempat dan tanggal lahir: ')
+    pekerjaan = input('input pekerjaan: ')
+    status = input('input status: ')
+ 
+    print('================================')
+    print('Biodata anda adalah: ')
+    print('nama: ' + nama)
+    print('alamat: ' + alamat)
+    print('ttl: ' + ttl)
+    print('pekerjaan: ' + pekerjaan)
+    print('status: ' + status)
+ 
+if __name__ == '__main__':
+    run()
